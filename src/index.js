2889a91e5ca72dcd57d4721339a8e152a258b50d
Yeong Sheng, Tan <tanyeongsheng@gmail.com>
2019-09-27 12:44:34 +0800

    count number of active WebSocket connections created per subscription

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ const { RedisCache } = require('apollo-server-cache-redis');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
+let numWSConnections = 0;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  subscriptions: {
+    onConnect: (connectionParams, webSocket) => {
+      numWSConnections += 1;
+      console.table(numWSConnections);
+    },
+  },
   cache: new RedisCache({
     host: 'localhost',
   }),
